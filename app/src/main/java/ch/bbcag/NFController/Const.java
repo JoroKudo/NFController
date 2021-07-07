d4bd88bfdd0d4cd417bae0d3a1c86feda62db433
Joro_Kudo <zfessj@bbcag.ch>
2021-07-07 12:32:12 +0200

    ☃ Db Procedures work ☃ ☃ write to tag button works again ☃

--- app/src/main/java/ch/bbcag/NFController/Const.java
+++ app/src/main/java/ch/bbcag/NFController/Const.java
@@ -13,12 +13,13 @@ import java.util.List;
 public class Const {
 
     public static final String[] fulltask = {"", "", "", "", "", "", "", "", "", ""};
-    public static  ArrayList<String[]> taskcontainer = new ArrayList<>();
+    public static ArrayList<String[]> taskcontainer = new ArrayList<>();
     public static final String SPACER = "∣";
     public static final List<String> procedures = new RequestHandler().insane();
 
     public static final String[] tasknames = {"BlueTooth", "WiFi", "TONE", "MUTE",
-            "VIBRATE", "VOL", "TTS", "OpenApp", "set Alarm", "set Timer", "Location", "FLASHLIGHT", "SendWhatsapp", "OpenWebsite", "Geofencing","Custom procedure"};
+            "VIBRATE", "VOL", "TTS", "OpenApp", "set Alarm", "set Timer", "Location", "FLASHLIGHT", "SendWhatsapp", "OpenWebsite", "Geofencing", "Custom procedure"};
+
     public static final List<String> TASKS = Arrays.asList("blue", "wifi", "tone", "mute", "vibrate", "vol", "tts", "open", "alarm", "timer", "location", "flash", "send", "web", "geofencing");
     public static final String[] GEOTASKS = {"blue", "blue", "wifi", "wifi", "tone", "mute", "vibrate", "vol", "open", "flash", "web"};
 
